7657cf814c4a65d14251c369b1b2fa94ef7c6039
emilsonsn <emilsonsn2@gmail.com>
2024-12-27 15:47:56 -0300

    fix: retira tabelas que não serão utilizadas

--- src/app/shared/tables/tables.module.ts
+++ src/app/shared/tables/tables.module.ts
@@ -3,15 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatIconButton } from "@angular/material/button";
-import { TableOrdersComponent } from './table-orders/table-orders.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRippleModule } from '@angular/material/core';
 import { PipesModule } from '@shared/pipes/pipes.module';
-import { TableProviderComponent } from './table-provider/table-provider.component';
 import { TableServiceComponent } from './table-service/table-service.component';
-import { TableConstructionComponent } from './table-construction/table-construction.component';
 import { TableClientComponent } from './table-client/table-client.component';
 import { TableRequestComponent } from './table-request/table-request.component';
 import { TableTypeProviderComponent } from './table-type-provider/table-type-provider.component';
@@ -22,11 +19,8 @@ import {SharedModule} from "@shared/shared.module";
 import {AvatarModule} from "@shared/components/avatar/avatar.module";
 
 const tables = [
-  TableOrdersComponent,
-  TableProviderComponent,
   TableServiceComponent,
   TableClientComponent,
-  TableConstructionComponent,
   TableRequestComponent,
   TableTypeProviderComponent,
   TableTypeProviderComponent,
