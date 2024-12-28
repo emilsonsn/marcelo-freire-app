62aa555ec48382d14425cc22f5dc9de22d22aa59
emilsonsn <emilsonsn2@gmail.com>
2024-12-28 15:47:49 -0300

    fix: corrige modulo de serviço

--- src/app/views/private/services/services.module.ts
+++ src/app/views/private/services/services.module.ts
@@ -5,8 +5,12 @@ import { ServicesRoutingModule } from './services-routing.module';
 import { ServicesComponent } from './services/services.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { SharedModule } from '@shared/shared.module';
-import {MatRipple} from "@angular/material/core";
+import {MatOptionModule, MatRipple} from "@angular/material/core";
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 
 @NgModule({
@@ -19,7 +23,12 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     SharedModule,
     MatDialogModule,
     MatRipple,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatOptionModule,
+    MatSelectModule
   ]
 })
 export class ServicesModule { }
