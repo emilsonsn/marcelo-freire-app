2a2675b369bba0277b547eeb0bf9c7d2d6a0109a
emilsonsn <emilsonsn2@gmail.com>
2025-01-07 18:43:42 -0300

    fix: configuração temporária de API

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,7 +4,8 @@ export const environment = {
   production: false,
   appName: 'Marcelo Freire',
   home: '/painel',
-  api: 'http://127.0.0.1:8000/api',
+  // api: 'http://127.0.0.1:8000/api',
+  api: 'http://145.223.27.103:3001/api',
   version: require('../../package.json').version
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@ export const environment = {
   production: false,
   appName: 'Marcelo Freire',
   home: '/painel/home',
-  api: 'http://127.0.0.1:8000/api',
+  // api: 'http://127.0.0.1:8000/api',
+  api: 'http://145.223.27.103:3001/api',
   version: require('../../package.json').version
 };
