360b881816d4eb66038836f563ff3dfc783667d0
emilsonsn <emilsonsn2@gmail.com>
2025-01-08 17:30:37 -0300

    fix: corrige enviroments

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -5,7 +5,7 @@ export const environment = {
   appName: 'Marcelo Freire',
   home: '/painel',
   // api: 'http://127.0.0.1:8000/api',
-  api: 'http://145.223.27.103:3001/api',
+  api: 'https://naturalmidiacloud.com.br:3001/api',
   version: require('../../package.json').version
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@ export const environment = {
   appName: 'Marcelo Freire',
   home: '/painel/home',
   // api: 'http://127.0.0.1:8000/api',
-  api: 'http://145.223.27.103:3001/api',
+  api: 'https://naturalmidiacloud.com.br:3001/api',
   version: require('../../package.json').version
 };
